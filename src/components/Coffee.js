e55a2db4d15afc7d7327c55b6f1be21110ea7732
OLGON92 <ogon318@gmail.com>
2023-03-25 19:44:01 -0700

    update coffee.js by pruning elements, and add a sell button

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -8,9 +8,8 @@ function Coffee(props) {
         <h1>Paisa Coffee Inventory Manager</h1>
         <h3>Name of Coffee: {props.name}</h3>
         <h3>Origin of Coffee: {props.origin}</h3>
-        <h3>Roast Type: {props.roast}</h3>
-        <h3>Price per Pound: ${props.price}</h3>
         <h3>Number of Pounds Left in Current Sack: {props.beansLeft}</h3>
+        <button onClick={()=> props.whenSellClicked(props.id) }>Sell a Pound?</button>
       </div>
     </React.Fragment>
   );
@@ -19,8 +18,6 @@ function Coffee(props) {
 Coffee.propTypes = {
   name: PropTypes.string,
   origin: PropTypes.string,
-  roast: PropTypes.string,
-  price: PropTypes.number,
   beansLeft: PropTypes.number,
   id: PropTypes.string,
   whenCoffeeClicked: PropTypes.func
