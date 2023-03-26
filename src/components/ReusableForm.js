e0f564c4a67b60e6984fd42875bf3e2a7ee6ea14
OLGON92 <ogon318@gmail.com>
2023-03-26 01:40:54 -0700

    make entries a requirement

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -8,23 +8,28 @@ function ReusableForm(props) {
         <input
           type='text'
           name='name'
-          placeholder="Coffee Name" />
+          placeholder="Coffee Name" 
+          required />
         <input
           type='text'
           name='origin'
-          placeholder="Coffee Origin" />
+          placeholder="Coffee Origin" 
+          required />
         <input
           type='text'
           name='roast'
-          placeholder="Coffee Roast e.g.light" />
+          placeholder="Coffee Roast e.g.light" 
+          required />
         <input
           type='number'
           name='price'
-          placeholder="Coffee Price in USD" />
+          placeholder="Coffee Price in USD" 
+          required />
         <input
           type='number'
           name='beansLeft'
-          placeholder="Total amount of beans in pounds" />
+          placeholder="Total amount of beans in pounds" 
+          required />
         <button type="submit">{props.buttonText}</button>
       </form>
     </React.Fragment>
